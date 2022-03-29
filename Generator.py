13a52c7e397ac0a03a5f8b64f7d0e5b66391285b
ldankert <ldankert@uni-osnabrueck.de>
2022-03-29 20:59:13 +0200

    add Generator and small Discriminator changes

--- Generator.py
+++ Generator.py
@@ -1,26 +1,21 @@
 """
-This is the class of the Discriminator, for the GAN model
+This is the class of the Generator, for the GAN model
 
-Created: 29.03.22, 19:00
+Created: 29.03.22, 20:20
 
 Author: LDankert
 """
-
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Bidirectional, Dense, LSTM
 
+from tensorflow.keras.layers import Input, LSTM, Dense, LeakyReLU, Reshape
 from util import NOTES_LENGTH, DRUM_CLASSES
-
 
 sequence_length = NOTES_LENGTH  # The length of the incoming drum matrices sequences
 
 nb_notes = len(DRUM_CLASSES)  # Number of possible notes
 
-droprate = 0
-
-
-class Discriminator(tf.keras.Model):
-    """Discriminator part of the GAN model"""
+class Generator(tf.keras.Model):
+    """Generator part of the GAN model"""
 
     def __init__(self, optimizer=tf.keras.optimizers.RMSprop(lr=0.0004)):
         """ Initializer
@@ -28,33 +23,36 @@ class Discriminator(tf.keras.Model):
         :param optimizer: (tf.keras.optimizers.Optimizer) optimizer to use in training,
                         default RMSprop with learing rate 0.0004.
         """
-        super(Discriminator, self).__init__()
+        super(Generator, self).__init__()
 
         self.optimizer = optimizer
 
         self.loss_function = tf.keras.losses.BinaryCrossentropy()
 
-        self.input_layer = Input(shape=(sequence_length, nb_notes))
-        self.all_layers = [
-            Bidirectional(LSTM(64, return_sequences=True, activation="tanh",
-                                             dropout=droprate, recurrent_dropout=droprate)),
-            Bidirectional(LSTM(64, return_sequences=True, activation="tanh",
-                                             dropout=droprate, recurrent_dropout=droprate)),
-            Dense(1, activation="sigmoid")
-        ]
+        L2_lambda = 0.01
+        dropout_rate = 0.2
 
+        self.input_layer = Input(shape=(sequence_length*nb_notes,))
+        self.all_layers = [
+            Dense(1024),
+            LeakyReLU(alpha=0.2),
+            Reshape((32,32)),
+            LSTM(512, return_sequences=True, activation="tanh"),
+            LSTM(512, return_sequences=True, activation="tanh"),
+            LSTM(9, return_sequences=True, activation="sigmoid")
+        ]
         self.out = self.call(self.input_layer, training=True)
 
-    def call(self, x, training=False):
+    def call(self, x, training):
         """ Call function of the model. Propagates input through the layers
 
         :param x: (tensor) input to the model
         :param training: (boolean) whether to use training or inference mode, default: False (inference)
         :return: (tensor) tensor x after running through the model
         """
-        for layers in self.all_layers:
+        for layer in self.all_layers:
             try:
-                x = layers(x, training=training)
+                x = layer(x, training)
             except:
-                x = layers(x)
+                x = layer(x)
         return x
